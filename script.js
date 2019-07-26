bb9865543c49d0415a251f99403f37f80aea0cc9
angryaxi <angryaxi@users.noreply.github.com>
2019-07-26 17:54:45 +0300

    Rewritten with modern javascript and better practices.

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-// otsikko
-var viive = 100;
-var time = 2500;
-function type(letter) {
-  document.getElementById("title").innerHTML += letter;
-  console.log("Kirjain ["+letter+"] kirjoitettu.");
-}
+const startupTime = 2500;
+const delay = 100;
+const message = 'angryaxi';
+const element = document.getElementById('title');
 
-function write() {
-  setTimeout(function(){ type("a"); }, time);
-  time += viive;
-  setTimeout(function(){ type("n"); }, time);
-  time += viive;
-  setTimeout(function(){ type("g"); }, time);
-  time += viive;
-  setTimeout(function(){ type("r"); }, time);
-  time += viive;
-  setTimeout(function(){ type("y"); }, time);
-  time += viive;
-  setTimeout(function(){ type("a"); }, time);
-  time += viive;
-  setTimeout(function(){ type("x"); }, time);
-  time += viive;
-  setTimeout(function(){ type("i"); }, time);
-  console.log("Aloitetaan otsikon kirjoittaminen.");
-}
-
-write();
+setTimeout(
+    async () => {
+        for (const letter of message.split('')) {
+            await new Promise(resolve => {
+                setTimeout(() => {
+                    element.innerHTML += letter;
+                    resolve();
+                }, delay)
+            })
+        }
+    }, startupTime
+);
